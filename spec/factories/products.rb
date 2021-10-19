f1a7d739ff5f2b752558cf517d07832bcb603dbc
João Vitor Alves <joao.vitor@redealumni.com>
2021-10-19 10:52:45 -0300

    Fix product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :product do
     name { "MyString" }
-    price_in_cents { 1 }
     image { "MyString" }
     short_description { "MyString" }
     description { "MyText" }
