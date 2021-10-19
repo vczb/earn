74f2afe374426c3c685ed8e89741868675f87fb4
João Vitor Alves <joao.vitor@redealumni.com>
2021-10-19 10:57:06 -0300

    Add test to Customer and Transaction

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:phone) }
+    it { is_expected.to validate_presence_of(:gender) }
+    it { is_expected.to validate_presence_of(:cpf) }
+    it { is_expected.to validate_presence_of(:birthday) }
+    it { is_expected.to validate_presence_of(:user_id) }
+  end
+
+  context 'enums' do
+    it { is_expected.to define_enum_for(:gender).with_values([:male, :female, :other]) }
+  end
+
+  context 'relations' do
+    it { is_expected.to belong_to(:user) }
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:price_in_cents) }
+    it { is_expected.to validate_presence_of(:price_in_diamonds) }
+    it { is_expected.to validate_presence_of(:transaction_type) }
+  end
+
+  context 'enums' do
+    it { is_expected.to define_enum_for(:transaction_type).with_values([:purchases, :withdrawals]) }
+  end
+
+  context 'relations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:product) }
+  end
 end
