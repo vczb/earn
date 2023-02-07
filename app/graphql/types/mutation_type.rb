f41e6d94c06af7f15fa4c6c4a1510b10d2e6c64d
Vinicius Zucatti <viniciuszucatti@gmail.com>
2023-02-06 22:23:26 -0300

    Create sign_in and create_user mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,7 @@ module Types
     field :create_product, mutation: Mutations::CreateProduct
     field :update_product, mutation: Mutations::UpdateProduct
     field :delete_product, mutation: Mutations::DeleteProduct
+    field :create_user, mutation: Mutations::CreateUser
+    field :sign_in_user, mutation: Mutations::SignInUser
   end
 end
