1473fe277d945d13bd603703e18d2f97529e2b5f
Vinicius Zucatti <viniciuszucatti@gmail.com>
2023-05-02 21:51:47 -0300

    Fix CI

--- app/graphql/mutations/delete_product.rb
+++ app/graphql/mutations/delete_product.rb
@@ -1,15 +1,14 @@
 module Mutations
   class DeleteProduct < BaseMutation
-
-  argument :id, Integer, required: true
+    argument :id, Integer, required: true
 
     type Types::ProductType
 
-      def resolve(id: nil)
-        user_id = context[:current_user].id
-        product = Product.where(user_id: user_id, id: id).first
-        product.destroy if product.present?
-        product
-      end
+    def resolve(id: nil)
+      user_id = context[:current_user].id
+      product = Product.where(user_id: user_id, id: id).first
+      product.destroy if product.present?
+      product
+    end
   end
 end
--- app/graphql/mutations/update_product.rb
+++ app/graphql/mutations/update_product.rb
@@ -9,22 +9,29 @@ module Mutations
 
     type Types::ProductType
 
-    def resolve(id:, name: nil, price_in_diamonds: nil, image: nil, short_description: nil, description: nil)
+    def resolve(
+      id:,
+      name: nil,
+      price_in_diamonds: nil,
+      image: nil,
+      short_description: nil,
+      description: nil
+    )
       product = Product.where(id: id, user_id: context[:current_user].id).first
 
       if product.present?
-
         product.update(
           name: name.presence || product.name,
-          price_in_diamonds: price_in_diamonds.presence || product.price_in_diamonds,
+          price_in_diamonds:
+            price_in_diamonds.presence || product.price_in_diamonds,
           image: image.presence || product.image,
-          short_description: short_description.presence || product.short_description,
+          short_description:
+            short_description.presence || product.short_description,
           description: description.presence || product.description
         )
         product
-
-      else 
-        {message: 'Product not found'}
+      else
+        { message: 'Product not found' }
       end
     end
   end
