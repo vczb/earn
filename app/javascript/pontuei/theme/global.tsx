75ad2c0c8c1697cb6c8ca46bd289b7792e0d64eb
Vini <viniczb@gmail.com>
2024-01-23 20:24:57 +0100

    Fix webkit typo issue

--- app/javascript/pontuei/theme/global.tsx
+++ app/javascript/pontuei/theme/global.tsx
@@ -25,11 +25,11 @@ export const globalStyles = (
         width: "0.4em",
       },
       "*::-webkit-scrollbar-track": {
-        "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,0.00)",
+          WebkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
       },
       "*::-webkit-scrollbar-thumb": {
-        backgroundColor: "rgba(0,0,0,.1)",
-        outline: "1px solid slategrey",
+          backgroundColor: "rgba(0,0,0,.1)",
+          outline: "1px solid slategrey",
       },
     }}
   />
