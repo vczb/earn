77996afce68f1f7b0ebd3f45811757fa32c37eb4
Vini <viniczb@gmail.com>
2024-01-24 16:43:15 +0100

    Fix duplicated HTML main tag

--- app/javascript/pontuei/index.tsx
+++ app/javascript/pontuei/index.tsx
@@ -4,6 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 document.addEventListener('DOMContentLoaded', () => {
-  const rootEl = document.getElementById('app')
+  const rootEl = document.getElementById("root")
   ReactDOM.render(<App />, rootEl)
 })
